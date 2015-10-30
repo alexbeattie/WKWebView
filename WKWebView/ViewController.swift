461c6e92a9bd74836493e6663b8f572be1709c3d
Alex Beattie <alexanderbeattie@mac.com>
2015-10-30 06:48:49 -0700

    Initial Commit

--- WKWebView/ViewController.swift
+++ WKWebView/ViewController.swift
@@ -7,19 +7,61 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var container: UIView!
+    var webView: WKWebView!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        
+        webView = WKWebView()
+        container.addSubview(webView)
+        print(container.bounds.width)
+    
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        
+        // change frame after you add subview bc once you add a subview, it inherits the parent's cooridinate system
+        let frame = CGRectMake(0, 0, container.bounds.width, container.bounds.height)
+        webView.frame = frame
+
+        loadRequest("http://www.blackcrowes.com/")
+
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
+    //reusable function
+    func loadRequest(urlStr: String) {
 
+        let url = NSURL(string: urlStr)!
+        let request = NSURLRequest(URL: url)
+        webView.loadRequest(request)
+        
+    }
+    
+    @IBAction func loadBlackCrowes(sender: AnyObject) {
+        loadRequest("http://www.blackcrowes.com/")
+
+    }
+
+    @IBAction func loadPhish(sender: AnyObject) {
+        
+        loadRequest("http://phish.com/")
+    }
+    
+    @IBAction func loadAllmanBrothers(sender: AnyObject) {
+        loadRequest("http://allmanbrothersband.com/")
+        
+    }
 }
 
